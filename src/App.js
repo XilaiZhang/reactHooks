1e78f360ee8aa6fe62f6a670c657fc73f0ac9956
Xilai Zhang <xilaizhang@g.ucla.edu>
2021-01-07 17:00:31 +0800

    deploy netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+   {
+       title: 'What is React?',
+       content: 'React is frontend js framework'
+   },
+   {
+       title: 'why react?',
+       content: 'it is a favorite js library among engineers'
+   } 
+];
 
-export default App;
+const options = [
+    {
+        label: 'The color red',
+        value: 'red'
+    },
+    {
+        label: 'The color green',
+        value: 'green'
+    },
+    {
+        label: 'The color blue',
+        value: 'blue'
+    },
+];
+
+export default ()=>{
+    const[selected, setSelected] = useState(options[0]);
+    // const[showDropdown, setShowDropdown] = useState(true);
+
+    return (
+        <div>
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a color"
+                    selected={selected} 
+                    onSelectedChange={setSelected}
+                    options={options} 
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
+            {/* <button onClick={()=>setShowDropdown(!showDropdown)}> Toggle Dropdown</button>
+            {showDropdown ? 
+                <Dropdown 
+                    selected={selected} 
+                    onSelectedChange={setSelected}
+                    options={options} 
+                /> : null
+            }            */}
+        </div>
+    );
+};
+
